55a897a9285c5cd670f93bcdea2b78c89e462d01
Wellington Silva <wellington.71319@gmail.com>
2025-07-14 21:12:29 -0300

    Update tool code

--- tool_example_normal.R
+++ tool_example_normal.R
@@ -1,11 +1,13 @@
-set.seed(123)
+#set.seed(123)
 
-# Dimensionality
-d <- 5000
+# Dimensionality 
+d <- 50
 
 # Number of particles and steps
 N <- 1000
-T <- 50  # intermediate distributions
+T <- d  # intermediate distributions
+threshold <- 0.5
+alpha_0 <- 0.01
 
 # Data
 A <- matrix(rnorm(d * d, 0, 1 / sqrt(d)), d, d)
@@ -13,7 +15,7 @@ x_true <- rnorm(d)
 y <- A %*% x_true + rnorm(d)
 
 # Tempering schedule
-alpha_seq <- seq(0, 1, length.out = T)
+alpha_seq <- alpha_0 + (0:(d - 1)) * (1 - alpha_0) / d
 
 # Particle storage
 particles <- matrix(rnorm(N * d), N, d)
@@ -44,10 +46,13 @@ for (t in 2:T) {
   cat(sprintf("Step %d / %d - ESS = %.1f\n", t, T, ESS[t]))
   
   # Resample
-  ancestors <- sample(1:N, N, replace = TRUE, prob = w)
-  particles <- particles[ancestors, ]
-  logw <- rep(0, N)
+  if (ESS[t] < N * threshold) {
+    ancestors <- sample(1:N, N, replace = TRUE, prob = w)
+    particles <- particles[ancestors, ]
+    logw <- rep(0, N)
+  }
   
+
   # Move particles (random walk)
   particles <- particles + matrix(rnorm(N * d, 0, 0.1), N, d)
 }
@@ -64,3 +69,11 @@ abline(0, 1, col = "red")
 plot(1:T, ESS, type = "b", pch = 19,
      xlab = "SMC step", ylab = "Effective Sample Size",
      main = "ESS during SMC")
+
+
+# Check
+weighted_means <- apply(particles, 2, function(x) sum(x * w))
+weighted_sds <- apply(particles, 2, function(x) sqrt(sum(w * (x - sum(x * w))^2)))
+
+mean(weighted_means)
+mean(weighted_sds)
